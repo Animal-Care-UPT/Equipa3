d9f7c203737ec32f7c3771d9950e85f61d48cbae
saracanelas <sara.i.canelas@gmail.com>
2025-11-15 20:38:05 +0000

    Addapted register shelter animal to springboot

--- src/main/java/AnimalCareCentre/server/controller/ShelterAnimalController.java
+++ src/main/java/AnimalCareCentre/server/controller/ShelterAnimalController.java
@@ -1,18 +1,26 @@
 package AnimalCareCentre.server.controller;
 
+import AnimalCareCentre.server.model.ShelterAnimal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import AnimalCareCentre.server.service.ShelterAnimalService;
 
 @RestController
-@RequestMapping("/shelteranimals/")
+@RequestMapping("/shelteranimals")
 public class ShelterAnimalController {
 
   private final ShelterAnimalService shelterAnimalService;
 
   public ShelterAnimalController(ShelterAnimalService shelterAnimalService) {
     this.shelterAnimalService = shelterAnimalService;
+  }
+
+  @PostMapping
+    public ShelterAnimal registerAnimal(@RequestBody ShelterAnimal shelterAnimal) {
+      return shelterAnimalService.registerShelterAnimal(shelterAnimal);
   }
 
 }
